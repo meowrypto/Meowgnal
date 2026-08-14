4c53b7a54802c90afadd9a4bd419a4dd69ceb564
meowrypto <f.doosti@msn.com>
2026-08-14 18:59:08 +0300

    add custom title bar and theme support to DrawingPropertiesWindow

--- Meowgnal/Views/DrawingPropertiesWindow.xaml.cs
+++ Meowgnal/Views/DrawingPropertiesWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using Meowgnal.Models;
 using Drawing = Meowgnal.Models.Drawing;
@@ -102,6 +103,43 @@ public partial class DrawingPropertiesWindow : Window
             PointsPanel.Children.Add(row);
         }
     }
+
+    #region Custom title bar
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2) { ToggleMaximize(); return; }
+        if (WindowState == WindowState.Maximized)
+        {
+            var point = PointToScreen(e.GetPosition(this));
+            WindowState = WindowState.Normal;
+            Left = point.X - Width / 2;
+            Top = point.Y - 15;
+        }
+        DragMove();
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+    private void Maximize_Click(object sender, RoutedEventArgs e) => ToggleMaximize();
+
+    private void Close_Click(object sender, RoutedEventArgs e) => Close();
+
+    private void ToggleMaximize()
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            MaximizeButton.Content = "⛶";
+        }
+        else
+        {
+            WindowState = WindowState.Maximized;
+            MaximizeButton.Content = "❐";
+        }
+    }
+
+    #endregion
 
     private void UpdateSwatch()
     {
