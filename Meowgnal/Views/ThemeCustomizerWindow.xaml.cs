1079e054a337a4afa4c8cdb4d6103cea1bab7dcc
meowrypto <f.doosti@msn.com>
2026-08-14 19:20:58 +0300

    add custom title bar and theme support to PortfolioWindow

--- Meowgnal/Views/ThemeCustomizerWindow.xaml.cs
+++ Meowgnal/Views/ThemeCustomizerWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using Meowgnal.Models;
@@ -43,6 +44,43 @@ public partial class ThemeCustomizerWindow : Window
 
         RefreshAll();
     }
+
+    #region Custom title bar
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2) { ToggleMaximize(); return; }
+        if (WindowState == WindowState.Maximized)
+        {
+            var point = PointToScreen(e.GetPosition(this));
+            WindowState = WindowState.Normal;
+            Left = point.X - Width / 2;
+            Top = point.Y - 15;
+        }
+        DragMove();
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+    private void Maximize_Click(object sender, RoutedEventArgs e) => ToggleMaximize();
+
+    private void Close_Click(object sender, RoutedEventArgs e) => Close();
+
+    private void ToggleMaximize()
+    {
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+            MaximizeButton.Content = "⛶";
+        }
+        else
+        {
+            WindowState = WindowState.Maximized;
+            MaximizeButton.Content = "❐";
+        }
+    }
+
+    #endregion
 
     private void BuildPalette(WrapPanel panel, string key)
     {
